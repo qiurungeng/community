8387a43522dcb6cecc8267c95c7c3b71039a3f2f
qiurungeng <708801794@qq.com>
2019-10-13 20:29:45 +0800

    init repo

---
+++ src/main/java/com/study/community/MajiangCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.study.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajiangCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajiangCommunityApplication.class, args);
+    }
+
+}
